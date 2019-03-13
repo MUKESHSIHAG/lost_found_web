dff8350565ace7a0e99f0d3e1bccc11fcf99d3d0
mukeshsihag <sihagmukesh22@gmail.com>
2019-03-13 21:22:25 +0530

    changes

--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -4,10 +4,29 @@ const { ensureAuthenticated } = require('../config/auth');
 
 router.get("/", (req, res) => res.render("welcome"));
 
+//Home route
+router.get("/welcome", isLoggedIn, (req, res) => {
+    res.render("home", {
+        user: req.user, isLoggedIn: req.isAuthenticated()
+    }) 
+});
+
 //dashboard  route
 router.get("/dashboard", ensureAuthenticated, (req, res) => 
-res.render("dashboard", {
-    name: req.user.name
+    res.render("dashboard", {
+        name: req.user.name
 }));
+
+function isLoggedIn(req, res, next) {
+    if (!req.isAuthenticated()) {
+       req.isLogged = false
+       console.log(req.isLogged);
+       return next();
+    } else {
+        req.isLogged = true
+        return next();
+    }
+    res.redirect('/');
+}
 
 module.exports = router
